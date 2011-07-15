7472f38d1b46af9ed209eed0553d8028b1905263
Kevin Rockwood <kevin@rockwood.me>
2011-07-14 21:34:28 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage themename
+ */
+?>
+
+	</div><!-- #main  -->
+
+	<footer id="colophon" role="contentinfo">
+			<div id="site-generator">
+				<small>&copy Copyright <?php echo date('Y') . " " . esc_attr( get_bloginfo( 'name', 'display' ) ); ?> <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'themename' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'themename' ), 'WordPress' ); ?></a></small>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<!-- Grab Google CDN's jQuery. Fall back to local if necessary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.4.4.min.js"%3E%3C/script%3E'))</script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage themename
+ */
+
+/**
+ * Make theme available for translation
+ * Translations can be filed in the /languages/ directory
+ */
+load_theme_textdomain( 'themename', TEMPLATEPATH . '/languages' );
+
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if ( is_readable( $locale_file ) )
+	require_once( $locale_file );
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 640;
+
+if ( !is_admin() ) { // instruction to only load if it is not the admin area
+   // register your script location, dependencies and version
+   wp_register_script('custom_script',
+       get_bloginfo('template_directory') . '/js/custom_script.js',
+       array('name_of_script_dependencies'),
+       '1.0' );
+   // enqueue the script
+   wp_enqueue_script('custom_script');
+}
+
+/**
+ * Remove code from the <head>
+ */
+//remove_action('wp_head', 'rsd_link'); // Might be necessary if you or other people on this site use remote editors.
+//remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
+//remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
+//remove_action('wp_head', 'index_rel_link'); // Displays relations link for site index
+//remove_action('wp_head', 'wlwmanifest_link'); // Might be necessary if you or other people on this site use Windows Live Writer.
+//remove_action('wp_head', 'start_post_rel_link', 10, 0); // Start link
+//remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Prev link
+//remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0); // Display relational links for the posts adjacent to the current post.
+remove_filter( 'the_content', 'capital_P_dangit' ); // Get outta my Wordpress codez dangit!
+remove_filter( 'the_title', 'capital_P_dangit' );
+remove_filter( 'comment_text', 'capital_P_dangit' );
+// Hide the version of WordPress you're running from source and RSS feed // Want to JUST remove it from the source? Try: remove_action('wp_head', 'wp_generator');
+/*function hcwp_remove_version() {return '';}
+add_filter('the_generator', 'hcwp_remove_version');*/
+// This function removes the comment inline css
+/*function twentyten_remove_recent_comments_style() {
+	global $wp_widget_factory;
+	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+}
+add_action( 'widgets_init', 'twentyten_remove_recent_comments_style' );*/
+
+/**
+ * Remove meta boxes from Post and Page Screens
+ */
+function customize_meta_boxes() {
+   /* These remove meta boxes from POSTS */
+  //remove_post_type_support("post","excerpt"); //Remove Excerpt Support
+  //remove_post_type_support("post","author"); //Remove Author Support
+  //remove_post_type_support("post","revisions"); //Remove Revision Support
+  //remove_post_type_support("post","comments"); //Remove Comments Support
+  //remove_post_type_support("post","trackbacks"); //Remove trackbacks Support
+  //remove_post_type_support("post","editor"); //Remove Editor Support
+  //remove_post_type_support("post","custom-fields"); //Remove custom-fields Support
+  //remove_post_type_support("post","title"); //Remove Title Support
+
+  
+  /* These remove meta boxes from PAGES */
+  //remove_post_type_support("page","revisions"); //Remove Revision Support
+  //remove_post_type_support("page","comments"); //Remove Comments Support
+  //remove_post_type_support("page","author"); //Remove Author Support
+  //remove_post_type_support("page","trackbacks"); //Remove trackbacks Support
+  //remove_post_type_support("page","custom-fields"); //Remove custom-fields Support
+  
+}
+add_action('admin_init','customize_meta_boxes');
+
+/**
+ * This theme uses wp_nav_menus() for the header menu, utility menu and footer menu.
+ */
+register_nav_menus( array(
+	'primary' => __( 'Primary Menu', 'themename' ),
+	'footer' => __( 'Footer Menu', 'themename' ),
+	'utility' => __( 'Utility Menu', 'themename' )
+) );
+
+/** 
+ * Add default posts and comments RSS feed links to head
+ */
+add_theme_support( 'automatic-feed-links' );
+
+/**
+ * This theme uses post thumbnails
+ */
+add_theme_support( 'post-thumbnails' );
+
+/**
+ *	This theme supports editor styles
+ */
+
+add_editor_style("/css/layout-style.css");
+
+/**
+ * Disable the admin bar in 3.1
+ */
+//show_admin_bar( false );
+
+/**
+ * This enables post formats. If you use this, make sure to delete any that you aren't going to use.
+ */
+//add_theme_support( 'post-formats', array( 'aside', 'audio', 'image', 'video', 'gallery', 'chat', 'link', 'quote', 'status' ) );
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ */
+function handcraftedwp_widgets_init() {
+	register_sidebar( array (
+		'name' => __( 'Sidebar', 'themename' ),
+		'id' => 'sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s" role="complementary">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h4 class="widget-title">',
+		'after_title' => '</h4>',
+	) );
+}
+add_action( 'init', 'handcraftedwp_widgets_init' );
+
+/*
+ * Remove senseless dashboard widgets for non-admins. (Un)Comment or delete as you wish.
+ */
+function remove_dashboard_widgets() {
+	global $wp_meta_boxes;
+
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']); // Plugins widget
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']); // WordPress Blog widget
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']); // Other WordPress News widget
+	//unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']); // Right Now widget
+	//unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']); // Quick Press widget
+	//unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']); // Incoming Links widget
+	//unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']); // Recent Drafts widget
+	//unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']); // Recent Comments widget
+}
+
+/**
+ *	Hide Menu Items
+ */
+function themename_configure_menu_page(){
+	
+	//remove_menu_page("link-manager.php"); //Hide Links
+	//remove_menu_page("edit-comments.php"); //Hide Comments
+	//remove_menu_page("tools.php"); //Hide Tools
+
+}
+
+if (!current_user_can('manage_options')) {
+	add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
+	add_action("admin_menu","themename_configure_menu_page"); //While we're add it, let's configure the menu options as well
+} 
+
+
+
+?>
+<?php // asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet
+//	 change the UA-XXXXX-X to be your site's ID
+/*add_action('wp_footer', 'async_google_analytics');
+function async_google_analytics() { ?>
+	<script>
+	var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+		(function(d, t) {
+			var g = d.createElement(t),
+				s = d.getElementsByTagName(t)[0];
+			g.async = true;
+			g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g, s);
+		})(document, 'script');
+	</script>
+<?php }*/ ?>
+<?php /*
+ * A default custom post type. DELETE from here to the end if you don't want any custom post types
+ */
+/*add_action('init', 'create_boilertemplate_cpt');
+function create_boilertemplate_cpt() 
+{
+  $labels = array(
+    'name' => _x('HandcraftedWPTemplate CPT', 'post type general name'),
+    'singular_name' => _x('HandcraftedWPTemplate CPT Item', 'post type singular name'),
+    'add_new' => _x('Add New', 'handcraftedwptemplate_robot'),
+    'add_new_item' => __('Add New Item'),
+    'edit_item' => __('Edit Item'),
+    'new_item' => __('New Item'),
+    'view_item' => __('View Item'),
+    'search_items' => __('Search Items'),
+    'not_found' =>  __('No items found'),
+    'not_found_in_trash' => __('No items found in Trash'), 
+    'parent_item_colon' => ''
+  );
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'show_ui' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'page',
+    'hierarchical' => false,
+    'menu_position' => 20,
+    'supports' => array('title','editor')
+  ); 
+  register_post_type('handcraftedwptemplate_robot',$args);
+}*/
+/*
+ * This is for a custom icon with a colored hover state for your custom post types. You can download the custom icons here 
+ http://randyjensenonline.com/thoughts/wordpress-custom-post-type-fugue-icons/
+ */
+/*add_action( 'admin_head', 'cpt_icons' );
+function cpt_icons() {
+    ?>
+    <style type="text/css" media="screen">
+        #menu-posts-handcraftedwptemplaterobot .wp-menu-image {
+            background: url(<?php bloginfo('template_url') ?>/images/robot.png) no-repeat 6px -17px !important;
+        }
+		#menu-posts-handcraftedwptemplaterobot:hover .wp-menu-image, #menu-posts-handcraftedwptemplaterobot.wp-has-current-submenu .wp-menu-image {
+            background-position:6px 7px!important;
+        }
+    </style>
+<?php }*/ ?>
